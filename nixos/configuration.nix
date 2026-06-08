3eaa86076e508394e21e5ed2369393f07d21ea3a
upalbarua <upalbarua52@gmail.com>
2026-06-08 09:47:09 +0600

    fix: font-config for bengali fonts

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -41,16 +41,47 @@
     powerOnBoot = false;
   };
 
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-color-emoji
-    nerd-fonts.iosevka
-    dejavu_fonts
-    liberation_ttf
-  ];
+  fonts = {
+    fontconfig = {
+      enable = true;
 
+      defaultFonts = {
+        serif = [
+          "Noto Serif"
+          "Noto Serif Bengali"
+        ];
+        sansSerif = [
+          "Noto Sans"
+          "Noto Sans Bengali"
+        ];
+        monospace = [ "Noto Sans Mono" ];
+      };
+
+      antialias = true;
+      useEmbeddedBitmaps = false;
+
+      hinting = {
+        enable = true;
+        autohint = false;
+        style = "slight";
+      };
+
+      subpixel = {
+        rgba = "rgb";
+        lcdfilter = "default";
+      };
+    };
+
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      nerd-fonts.iosevka
+      dejavu_fonts
+      liberation_ttf
+    ];
+  };
   services.fstrim.enable = true;
 
   # Sound.
